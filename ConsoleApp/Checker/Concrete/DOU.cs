295591b1b761cdbc8f1735bc0c82d97c81ea6a09
Nikita <dev.potapy4@hotmail.com>
2017-07-06 15:44:27 +0300

    Small fix

--- ConsoleApp/Checker/Concrete/DOU.cs
+++ ConsoleApp/Checker/Concrete/DOU.cs
@@ -12,7 +12,7 @@ namespace ConsoleApp.Checker.Concrete
             request.Method = "HEAD"; // For status code only, without body response
             var response = (HttpWebResponse)request.GetResponse();
 
-            return (int)response.StatusCode == 200 ? "DOU.UA ONLINE" : "DOU.UA OFFLINE";
+            return (int)response.StatusCode == 200 ? "DOU.UA ONLINE" : $"DOU.UA RETURNED STATUS CODE: {response.StatusCode}";
         }
     }
 }
